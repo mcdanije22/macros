5434c2aee2fe282e6966cf70a3a57c1fb78ea5d9
mcdanije22 <mcdanije22@gmail.com>
2020-04-21 15:50:21 -0400

    deploy

--- server.js
+++ server.js
@@ -42,9 +42,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "client", ".next", "build", "index.html")
-    );
+    res.sendFile(path.resolve(__dirname, "client", ".next", "index.html"));
   });
 }
 

27fdfe58b0a73fe60731ab5794168f4485a72647
paramagicdev <konnor5456@gmail.com>
2020-04-22 16:45:06 -0400

    pulling in default settings

--- server.js
+++ server.js
@@ -1,49 +1,61 @@
-// import express from "express";
-// import bodyParser from "body-parser";
-// import cors from "cors";
-// import mongoose from "mongoose";
-// import path from "path";
+// const express = require('express')
+// const bodyParser = require('body-parser')
+// const cors = require('cors')
+// const mongoose = require('mongoose')
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const path = require('path')
+// const app = express()
+// const PORT = process.env.PORT || 5000
 
-const dburl =
+// app.use(bodyParser.json())
+// app.use(cors())
+
+// app.listen(PORT, () => console.log(`server started successfully on ${PORT}`))
+
+const dburi =
   process.env.MONGODB_URI ||
   'mongodb+srv://josh:josh123@macrosocial-yeplw.mongodb.net/test?retryWrites=true&w=majority'
 
-mongoose.connect(dburl, { useNewUrlParser: true }, () => {
+mongoose.connect(dburi, { useNewUrlParser: true }, () => {
   console.log('db connected')
 })
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const express = require('express')
+const next = require('next')
 
-app.use(bodyParser.json())
-app.use(cors())
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
-// import postsRoutes from "./routes/postRoutes";
-// import userRoutes from "./routes/userRoutes";
-// import searchPostsRoutes from "./routes/searchPostsRoutes";
+// const postsRoutes = require('./routes/postRoutes')
+// const userRoutes = require('./routes/userRoutes')
+// const searchPostsRoutes = require('./routes/searchPostsRoutes')
 
-const postsRoutes = require('./routes/postRoutes')
-const userRoutes = require('./routes/userRoutes')
-const searchPostsRoutes = require('./routes/searchPostsRoutes')
+// app.use('/foodposts', postsRoutes)
+// app.use('/users', userRoutes)
+// app.use('/searchposts', searchPostsRoutes)
 
-app.use('/foodposts', postsRoutes)
-app.use('/users', userRoutes)
-app.use('/searchposts', searchPostsRoutes)
+app.prepare().then(() => {
+  const server = express()
 
-// // Serve static assets if in production
-// if (process.env.NODE_ENV === 'production') {
-//   // Set static folder
-//   app.use(express.static('client/build'))
+  server.get('/foodposts', (req, res) => {
+    return app.render(req, res, '/foodposts', req.query)
+  })
 
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//   })
-// }
+  server.get('/users', (req, res) => {
+    return app.render(req, res, '/users', req.query)
+  })
 
-app.listen(PORT, () => console.log(`server started successfully on ${PORT}`))
+  server.get('/searchposts', (req, res) => {
+    return app.render(req, res, '/searchposts', req.query)
+  })
+
+  server.all('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})

ff54d786525bf79255dc1126ab10cdb1427b62c4
paramagicdev <konnor5456@gmail.com>
2020-04-22 16:48:40 -0400

    redefine mongoose

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // const express = require('express')
 // const bodyParser = require('body-parser')
 // const cors = require('cors')
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
 // const app = express()
 // const PORT = process.env.PORT || 5000

f5facfca799bb2a432042e550f3d59603b47dee0
mcdanije22 <mcdanije22@gmail.com>
2020-04-22 11:55:54 -0400

    new folder structure for deploys

--- server.js
+++ server.js
@@ -4,46 +4,46 @@
 // import mongoose from "mongoose";
 // import path from "path";
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const path = require("path");
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const path = require('path')
 
 mongoose.connect(
-  "mongodb+srv://josh:josh123@macrosocial-yeplw.mongodb.net/test?retryWrites=true&w=majority",
+  'mongodb+srv://josh:josh123@macrosocial-yeplw.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true },
   () => {
-    console.log("db connected");
+    console.log('db connected')
   }
-);
+)
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+const app = express()
+const PORT = process.env.PORT || 5000
 
-app.use(bodyParser.json());
-app.use(cors());
+app.use(bodyParser.json())
+app.use(cors())
 
 // import postsRoutes from "./routes/postRoutes";
 // import userRoutes from "./routes/userRoutes";
 // import searchPostsRoutes from "./routes/searchPostsRoutes";
 
-const postsRoutes = require("./routes/postRoutes");
-const userRoutes = require("./routes/userRoutes");
-const searchPostsRoutes = require("./routes/searchPostsRoutes");
+const postsRoutes = require('./routes/postRoutes')
+const userRoutes = require('./routes/userRoutes')
+const searchPostsRoutes = require('./routes/searchPostsRoutes')
 
-app.use("/foodposts", postsRoutes);
-app.use("/users", userRoutes);
-app.use("/searchposts", searchPostsRoutes);
+app.use('/foodposts', postsRoutes)
+app.use('/users', userRoutes)
+app.use('/searchposts', searchPostsRoutes)
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static('client/build'))
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
-app.listen(PORT, () => console.log(`server started successfully on ${PORT}`));
+app.listen(PORT, () => console.log(`server started successfully on ${PORT}`))
